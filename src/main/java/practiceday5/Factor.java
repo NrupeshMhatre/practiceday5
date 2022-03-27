98e6653ec5f6b5eea2dd5713afe1ae89251f203d
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-03-27 13:27:24 +0530

    factor added

---
+++ src/main/java/practiceday5/Factor.java
@@ -0,0 +1,22 @@
+package practiceday5;
+
+import java.util.Scanner;
+
+public class Factor {
+
+	public static void main(String[] args) {
+		
+		int n;
+		System.out.println("Enter Any no");
+		Scanner r=new Scanner(System.in);
+		n=r.nextInt();
+		for(int i=1;i<=n;i++) {
+			
+			if(n%i==0) 
+			{
+				System.out.println(i+" ");
+			}
+		}
+	}
+
+}
